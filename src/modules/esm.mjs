8b93e25e3e16ec4b14cf2b3c730b224e0a99533e
Bhushan_Madaye <bhushan@123>
2024-03-27 20:16:43 +0530

    file refactored with ecmascript

---
+++ src/modules/esm.mjs
@@ -0,0 +1,49 @@
+// const path = require('path');
+// import * as path from 'node:path';
+// const { release, version } = require('os');
+import { release, version } from 'node:os';
+// const { createServer: createServerHttp } = require('http');
+import { createServer as createServerHttp } from 'node:http';
+// require('./files/c');
+import { someFunction } from './files/c.mjs';
+import * as a from './files/a.json' assert { type: 'json' };
+import * as b from './files/b.json' assert { type: 'json' };
+import { dirname, sep } from 'path'
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+    // unknownObject = require('./files/a.json');
+    unknownObject = a;
+} else {
+    // unknownObject = require('./files/b.json');
+    unknownObject = b;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${import.meta.url}`);
+console.log(`Path to current directory is ${dirname(import.meta.url)}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export const esm = {
+    unknownObject,
+    myServer,
+};
+
