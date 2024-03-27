92717172a323163274a5e2499a837ef54ae37fbb
Bhushan_Madaye <bhushan@123>
2024-03-27 17:30:56 +0530

    completed cli questions in node basics

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
     // Write your code here 
+    console.log(argv);
+    let finalOutput = '';
+    argv.forEach((element, index) => {
+        console.log(element);
+        if (element.startsWith('--')) {
+            finalOutput += `${element} is ${argv[index + 1]}, `;
+        }
+    });
+
+    finalOutput.trim();
+    const trimmedOutput = finalOutput.slice(0, -2);
+    console.log({trimmedOutput});
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,20 @@
 const parseEnv = () => {
     // Write your code here 
+    let rssKeys = [];
+    let finalOuput = '';
+    for (const key in process.env) {
+        if (Object.hasOwnProperty.call(process.env, key)) {
+            const value = process.env[key];
+            if (key.startsWith('RSS_')) {
+                console.log(key);
+                rssKeys.push(key);
+                finalOuput += `${key}=${value}; `;
+            }
+        }
+    }
+    // removing '; ' from last
+    const trimmedString = finalOuput.slice(0, -2);
+    console.log({trimmedString})
 };
 
 parseEnv();
