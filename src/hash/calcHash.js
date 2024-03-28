d8b56b6359835f8baaa120902ca5b5fb6ecbe8f8
Bhushan_Madaye <bhushan@123>
2024-03-28 16:49:08 +0530

    hashing completed

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+const { createHash } = await import('node:crypto');
+import { createReadStream } from 'node:fs';
+
 const calculateHash = async () => {
     // Write your code here 
+    const hash = createHash('sha256', { encoding: 'hex' });
+    const readStream = createReadStream('src/hash/files/fileToCalculateHashFor.txt');
+    readStream.pipe(hash).pipe(process.stdout);
 };
 
 await calculateHash();
