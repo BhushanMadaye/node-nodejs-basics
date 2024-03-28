14ba2e7b7fe009bfbbd7a0b7fb02e03c690d8998
Bhushan_Madaye <bhushan@123>
2024-03-28 15:46:39 +0530

    zlib completed

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const compress = async () => {
     // Write your code here 
+    const readableStream = createReadStream('src/zip/files/fileToCompress.txt', { encoding: 'utf8' });
+    const writeableStream = createWriteStream('src/zip/files/archieve.gz');
+    const compressionStream = createGzip();
+    readableStream.pipe(compressionStream).pipe(writeableStream);
+    
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip, createUnzip } from 'node:zlib'
+
 const decompress = async () => {
     // Write your code here 
+    const readableStream = createReadStream('src/zip/files/archieve.gz');
+    const writeableStream = createWriteStream('src/zip/files/fileToCompress.txt');
+    const decompressStream = createGunzip();
+
+    readableStream.pipe(decompressStream).pipe(writeableStream);
 };
 
 await decompress();
